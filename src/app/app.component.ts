8cb3b68ff5f62b4a935b318953ba006318abed38
Etnes <sente.quentin@gmail.com>
2023-11-18 22:25:45 +0100

    #adding primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToggleButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
